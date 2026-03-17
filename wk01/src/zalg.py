2bb8efbbeb0f7bbe55136294c9e09f3b24d8bb28
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-03-17 19:16:37 +1100

    wk01: fix zalg py

--- wk01/src/zalg.py
+++ wk01/src/zalg.py
@@ -3,8 +3,9 @@ def zalg(string: str) -> list[int]:
     n = len(string) # 18
     z = [0] * n
     z[0] = n
+    print('set 0th index to n')
 
-    left, right = 0, 0
+    left, right = -1, -1
 
     for k in range(1,n):
         k_mirror = k - left
@@ -12,31 +13,35 @@ def zalg(string: str) -> list[int]:
         
         # case 1 (k > r)
         if k > right:
+            print('case 1')
             i = 0
-            while k + i < n and string[i] == string[k + i]:
+            while right + i < n and string[i] == string[k + i]:
                 i += 1
             z[k] = i
             if z[k] > 0:
                 left = k
-                right = z[k]
+                right = k + z[k]
         # case 2 (k <= r)
         elif k <= right:
-            # 2a
+            # 2a: fully contained within zbox
             if z[k_mirror] < zbox_offset:
-                z[k] = z[k - left]
+                print('case 2a: reused previous value')
+                z[k] = z[k_mirror]
 
-            # 2b: 
-            elif z[k_mirror] > zbox_offset:
+            # 2b
+            elif z[k_mirror] >= zbox_offset:
+                print('case 2b + possibly 2c')
                 z[k] = right - k
                 # 2c: extend past right of zbox
-                if z[k_mirror] == zbox_offset:
-                    i = 0
-                    while k - i < n and string[right + i] == string[right - k + i]:
-                        i += 1
-                    z[k] += i
-                    if z[k] > 0:
-                        left = k
-                        right = k + z[k]
+                # if z[k_mirror] == zbox_offset:
+                i = 0
+                while right + i < n and string[right + i] == string[right - k + i]:
+                    i += 1
+                z[k] += i
+                if z[k] > 0:
+                    left = k
+                    right = k + z[k]
+
     
     return z
 
@@ -44,5 +49,16 @@ if __name__ == '__main__':
     z = zalg("aabcaabxaabcaabcay")
     print(z)
 
-    assert z[0] == 18
-    assert z == [18,1,0,0,3,1,0,0,7,1,0,0,5,1,0,0,1,0]
+    correct = [18,1,0,0,3,1,0,0,7,1,0,0,5,1,0,0,1,0]
+    assert z[0] == correct[0]
+    assert z == correct, f'\nExpected {correct},\nGot      {z}'
+
+    del z
+    del correct
+    print('\n')
+
+    z = zalg("abaaba")
+    print(z)
+    
+    correct = [6,0,1,3,0,1]
+    assert z == correct, f'\nExpected {correct},\nGot      {z}'
